3426a55f2f5eee3c2bfe3c4ee83a2e2d65683d8a
lepinoch <lepinoch3@gmail.com>
2015-10-12 23:43:34 -0300

    Base de datos sin relaciones y vista sin accion de producto
    Dia 1

---
+++ Proyectofinal2/Controllers/db_producto/ProductoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyectofinal2.Controllers.db_producto
+{
+    public class ProductoController : Controller
+    {
+        //
+        // GET: /Producto/
+
+        [HttpGet]
+        public ActionResult IngresoProducto()
+        {
+            return View();
+        }
+
+    }
+}
